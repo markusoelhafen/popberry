900a44ffb8077e8277c85c0c4fd9341937a24888
Markus Oelhafen <markus.oelhafen@sinnerschrader.com>
2017-04-13 23:13:51 +0200

    added functionality to read temperature from motion sensor

---
+++ huetest/hue_read.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+from ConfigParser import SafeConfigParser
+
+####################
+# READ CONFIG FILE #
+####################
+
+parser = SafeConfigParser()
+parser.read('config.ini')
+
+IP = parser.get('config', 'ip')
+USER = parser.get('config', 'user')
+TEMPSENSOR = parser.get('config', 'temp_sensor')
+
+#generate api urls
+geturl = "http://" + IP  + "/api/" + USER  + "/sensors/" + TEMPSENSOR
+
+
+#define json requests
+config_on = {"on":True}
+
+#get request from api
+g = requests.get(geturl)
+
+#print g
+#print g.json()['state']['on']
+
+if g.json()['config']['on']: #if light is already on -> turn off
+    print g.json()['state']['temperature']
+else: #else light is off -> turn on
+    r = requests.put(puturl, json.dumps(data_on), timeout=5)
+
+# i am the test comment
