34483d3b60eda026cf21bf1b19f4d39e01ddaea6
Markus Oelhafen <markus.oelhafen@sinnerschrader.com>
2017-04-14 23:09:21 +0200

    firebase - added config file and parser in pyrebase_test. removed unneeded .pyc files. added firebaseconfig to gitignore

--- firebase/pyrebase_test.py
+++ firebase/pyrebase_test.py
@@ -1,19 +1,35 @@
 #!/usr/local/bin/python3
 
 import pyrebase
+from configparser import SafeConfigParser
+
+####################
+# READ CONFIG FILE #
+####################
+
+parser = SafeConfigParser()
+parser.read('firebase_config.ini')
+
+APIKEY = parser.get('config', 'APIKEY')
+AUTHDOMAIN = parser.get('config', 'AUTHDOMAIN')
+DATABASEURL = parser.get('config', 'DATABASEURL')
+STORAGEBUCKET = parser.get('config', 'STORAGEBUCKET')
+SERVICEACCOUNT = parser.get('config', 'SERVICEACCOUNT')
+USER = parser.get('config', 'USER')
+PWD = parser.get('config', 'PWD')
 
 config = {
-  "apiKey": "AIzaSyA7vu2G0FdYDbBHz2meKvJTwoCk4Eq7y5M",
-  "authDomain": "popberry-11c0b.firebaseapp.com",
-  "databaseURL": "https://popberry-11c0b.firebaseio.com",
-  "storageBucket": "popberry-11c0b.appspot.com",
-  "serviceAccount": "./popberry-b68394b02e78.json"
+  "apiKey": APIKEY,
+  "authDomain": AUTHDOMAIN,
+  "databaseURL": DATABASEURL,
+  "storageBucket": STORAGEBUCKET,
+  "serviceAccount": SERVICEACCOUNT
 }
 
 firebase = pyrebase.initialize_app(config)
 
 auth = firebase.auth()
 #authenticate a user
-user = auth.sign_in_with_email_and_password("oelhafenmarkus@gmail.com", "67aq87x15")
+user = auth.sign_in_with_email_and_password(USER, PWD)
 
 print(user)
