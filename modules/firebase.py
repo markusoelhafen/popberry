0167277e5b8b66d1890fcf283854301c16e3e5ae
Markus Oelhafen <oelhafenmarkus@gmail.com>
2017-04-17 19:01:35 +0200

    hue - added temperature sensor function; added firebase functionality as module

---
+++ modules/firebase.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import pyrebase
+from configparser import SafeConfigParser
+
+parser = SafeConfigParser()
+parser.read('./config.ini')
+
+APIKEY = parser.get('firebase', 'apikey')
+AUTHDOMAIN = parser.get('firebase', 'authdomain')
+DATABASEURL = parser.get('firebase', 'databaseurl')
+STORAGEBUCKET = parser.get('firebase', 'storagebucket')
+SERVICEACCOUNT = parser.get('firebase', 'serviceAccount')
+FB_USER = parser.get('firebase', 'fb_user')
+FB_PWD = parser.get('firebase', 'fb_pwd')
+
+fb_config = {
+  "apiKey": APIKEY,
+  "authDomain": AUTHDOMAIN,
+  "databaseURL": DATABASEURL,
+  "storageBucket": STORAGEBUCKET,
+  "serviceAccount": SERVICEACCOUNT
+}
+
+firebase = pyrebase.initialize_app(fb_config)
+auth = firebase.auth()
+# authenticate user
+user = auth.sign_in_with_email_and_password(FB_USER, FB_PWD)
+# initialize database
+db = firebase.database()
+
+def pushToDatabase(data, children):
+    path = ''
+    for child in children:
+        path += "/{}".format(child.strip("/"))
+    db.child(path).set(data, user['idToken'])
--- popberry.py
+++ popberry.py
@@ -1,8 +1,5 @@
 #!/usr/local/bin/python3
 
-import requests
-import json
-import pyrebase
 from modules import *
 from configparser import SafeConfigParser
 
@@ -12,3 +9,12 @@ print(weather)
 lights = hue.getLights()
 for light, val in lights.items():
     print(val['name'])
+
+temp = hue.getTemperatureSensors(hue.getSensors())
+for sensor, val in temp.items():
+    room_name = val['name']
+    room_temp = val['state']['temperature']/100
+    print(room_name,': ',room_temp)
+
+    #push to database
+    firebase.pushToDatabase(room_temp, ['room_temp', room_name])
