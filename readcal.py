35e5029bec7ea976130dfa35e3a8eb99b13d92f3
Markus Oelhafen <oelhafenmarkus@gmail.com>
2017-04-21 19:04:55 +0200

    optimized getUpcomingEvents function, so that all specified calendars get parsed and displayed sorted by start time

--- readcal.py
+++ readcal.py
@@ -2,7 +2,6 @@ import httplib2
 from apiclient import discovery
 
 import datetime
-
 import quickstart
 
 def main():
@@ -10,19 +9,29 @@ def main():
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
 
-    getEventsOfTheDay('markus.oelhafen@sinnerschrader.com', service)
+    calendarIds = ['65lu3jc07jnt7k19tn4466qdbc@group.calendar.google.com', 'primary']
+
+    allEvents = getEventsOfTheDay(calendarIds, service)
+
+    for event in allEvents:
+        print(event['summary'], event['start']['dateTime'])
 
 def getEventsOfTheDay(calId, service):
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' idicates UTC time
-    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-    tomorrow = '2017-04-20T00:00:00Z'
-    print(now)
-    print(tomorrow)
-    eventsResult = service.events().list(
-        calendarId=calId, timeMin=now, timeMax=tomorrow, singleEvents=True, orderBy='startTime').execute()
-    events = eventsResult.get('items', [])
-    for event in events:
-        print(event['summary'])
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time #define current time
+    tomorrow = (datetime.datetime.utcnow() + datetime.timedelta(days=1)).isoformat() + 'Z' #define current time + 24h
+
+    eventlist = [] #create empty list
+
+    for cal in calId: #iterate through all specified calendarIDs
+        eventsResult = service.events().list(calendarId=cal, timeMin=now, timeMax=tomorrow, singleEvents=True, orderBy='startTime').execute() #call google calendar api
+        events = eventsResult.get('items', []) #save returned events in variable
+        for event in events: # iterate through calendar events
+            eventlist.append(event) # append the event to the eventlist list (this is still sorted calendar after calendar)
+
+    eventlist = sorted(eventlist, key=lambda event: event['start']['dateTime']) # sort the eventlist by the start time of an event
+    return eventlist
+
+    # return(eventlist)
 
 def getUpcomingEvents(resultCount):
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' idicates UTC time
@@ -42,7 +51,9 @@ def getUpcomingEvents(resultCount):
 def getCalendars(service):
     listResult = service.calendarList().list(pageToken=None).execute()
     lists = listResult.get('items', [])
-    print(lists)
+    # print(lists)
+    for list in lists:
+        print(list['summary'],': ',list['id'])
 
 if __name__ == '__main__':
     main()
